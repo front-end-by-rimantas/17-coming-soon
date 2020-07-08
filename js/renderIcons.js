b4356161806217f9d87951b6dd3cc6c18ff8ce30
Rimantas <belauzas@yahoo.com>
2020-07-08 13:28:30 +0300

    Social icons (links) #12

--- js/renderIcons.js
+++ js/renderIcons.js
@@ -1,1 +1,16 @@
-console.log('render icons...');
+function renderIcons( selector, icons ) {
+    const count = icons.length;
+    const DOM = document.querySelector(selector);
+    let HTML = '';
+
+    DOM.innerHTML = '';
+    
+    for ( let i=0; i<count; i++ ) {
+        const social = icons[i];
+        HTML += `<a href="${social.url}" class="fa fa-${social.icon}"></a>`;
+    }
+    
+    return DOM.innerHTML = HTML;
+}
+
+export default renderIcons;
